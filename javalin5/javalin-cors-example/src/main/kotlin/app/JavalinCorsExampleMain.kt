35eaf37d3b581c794b125b0c63562574889cf1a7
David <davidaase@hotmail.com>
2022-08-03 18:28:49 +0200

    fix cors example

---
+++ javalin5/javalin-cors-example/src/main/kotlin/app/JavalinCorsExampleMain.kt
@@ -0,0 +1,28 @@
+package app
+
+import io.javalin.Javalin
+
+fun main() {
+
+    Javalin.create {
+        it.plugins.enableCorsForAllOrigins()
+        it.plugins.enableDevLogging()
+    }.get("/") { ctx ->
+        ctx.result("Message from server on port 7070")
+    }.start(7070)
+
+    Javalin.create()
+        .get("/") {
+            it.html(
+                """
+                <script>
+                    fetch("http://localhost:7070")
+                        .then(response => response.text())
+                        .then(text => document.write("<h1>" + text + "</h1>"));
+                </script>
+                """.trimIndent()
+            )
+        }.start(7071)
+
+
+}
