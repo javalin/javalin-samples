f3ced85bb831a247ea77dfc803a6295f0c8d136f
David <davidaase@hotmail.com>
2022-08-03 19:18:58 +0200

    fix email example

---
+++ javalin5/javalin-email-example/src/main/java/JavalinEmailExampleApp.java
@@ -0,0 +1,45 @@
+import io.javalin.Javalin;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+import static j2html.TagCreator.br;
+import static j2html.TagCreator.button;
+import static j2html.TagCreator.form;
+import static j2html.TagCreator.input;
+import static j2html.TagCreator.textarea;
+
+public class JavalinEmailExampleApp {
+
+    public static void main(String[] args) {
+
+        Javalin.create()
+            .routes(() -> {
+                get("/", ctx -> ctx.html(
+                    form().withAction("/contact-us").withMethod("post").with(
+                        input().withName("subject").withPlaceholder("Subject"),
+                        br(),
+                        textarea().withName("message").withPlaceholder("Your message ..."),
+                        br(),
+                        button("Submit")
+                    ).render()
+                ));
+                post("/contact-us", ctx -> {
+                    Email email = new SimpleEmail();
+                    email.setHostName("smtp.googlemail.com");
+                    email.setSmtpPort(465);
+                    email.setAuthenticator(new DefaultAuthenticator("YOUR_EMAIL", "YOUR_PASSWORD"));
+                    email.setSSLOnConnect(true);
+                    email.setFrom("YOUR_EMAIL");
+                    email.setSubject(ctx.formParam("subject"));
+                    email.setMsg(ctx.formParam("message"));
+                    email.addTo("RECEIVING_EMAIL");
+                    email.send(); // will throw email-exception if something is wrong
+                    ctx.redirect("/contact-us/success");
+                });
+                get("/contact-us/success", ctx -> ctx.html("Your message was sent"));
+            }).start(7070);
+    }
+
+}
