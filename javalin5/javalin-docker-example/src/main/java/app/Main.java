75d9b54839c7985ec7616e818956ccbc483cd0ce
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-10-30 15:54:17 +0100

    Add docker example

---
+++ javalin5/javalin-docker-example/src/main/java/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
